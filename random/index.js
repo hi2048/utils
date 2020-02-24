b91830c99e9b9735b27cdb6f169d8032e178119c
hi2048 <291882199@qq.com>
2020-02-24 19:23:44 +0800

    extend random method.

--- random/index.js
+++ random/index.js
@@ -1,4 +1,31 @@
+const { isNumber, isUndefined } = require('../type');
+
+/**
+ * 
+ * @param {int} min default 1
+ * @param {int} max default 10
+ * 
+ * @return [min, max] random number
+ */
 const random = (min, max) => {
+  if(isUndefined(min) && isUndefined(max)) {
+    min = 1;
+    max = 10;
+  }
+
+  if(!isNumber(min)) {
+    throw new Error('Expected the parameter to be a number.');
+  }
+
+  if(isUndefined(max)) {
+    max = min;
+    min = max < 1?max : 1;
+  }
+
+  if(min > max) {
+    throw new Error('Expected arg2 larger than arg1.');
+  }
+
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
--- random/index.test.js
+++ random/index.test.js
@@ -3,5 +3,10 @@ const { isNumber, isArray, isString } = require('../type');
 const { random, getNRandomNumbers, prefixZero } = require('.');
 
 assert.ok(isNumber(random(1, 100)));
+assert.equal(random(1, 1), 1);
+assert.equal(0 < random(2) && random(2) < 3, true);
+assert.equal(0 < random() && random() < 11, true);
+assert.equal(0 < random(1, 2) && random(1, 2) < 3, true);
+
 assert.ok(isArray(getNRandomNumbers(10, {min: 1, max: 100})));
 assert.ok(isString(prefixZero(10, 5)));
